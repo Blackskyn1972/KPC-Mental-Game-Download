470b38c36a244170764d8f16428b97a3b660bfb8
Blackskyn1972 <Blackskyn@hotmail.fr>
2026-09-19 11:17:08 +0200

    Refresh KPC PWA cache strategy for V2.10

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const C="kpc-mental-v22";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./logo.svg"]))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)))})
+const C="kpc-mental-v210";
+const CORE=["./","./index.html","./manifest.webmanifest","./logo.svg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const x=r.clone();
+    caches.open(C).then(c=>c.put(e.request,x));
+    return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
+});
